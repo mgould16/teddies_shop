26809db8acf347a35f9eb7a79ec563a3f63de5e4
Mark Gould <m.gould16@gmail.com>
2019-03-17 16:28:35 +0000

    completion of lecture, now to kick up local host

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
