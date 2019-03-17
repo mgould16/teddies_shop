982a22535a4628d2569fb3c5eef047b5694e102d
Mark Gould <m.gould16@gmail.com>
2019-03-17 19:09:50 +0000

    Added pricing to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,7 @@ geek = Category.create!(name: 'geek')
 kids = Category.create!(name: 'kids')
 
 puts 'Creating teddies...'
-Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
-
-Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub',      category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', price_cents: 4000, category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
+Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', price_cents: 2000, category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub',  price_cents: 5000,    category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
 puts 'Finished!'
